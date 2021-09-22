906f1dd4ff9a788e3513eb8232790b57d23dafe3
Giovanni Malcolm <giovannimalcolm@giovannis-mbp.lan>
2021-09-22 18:11:17 -0400

    routine commit. stumped right now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 //starting variables 
-var startButton = document.querySelector(".start-button");
-var timerEl = document.queryselector('.timer')
-var startQuiz = document.queryselector('.startQuiz')
+var startBtnEl = document.querySelector(".start-button");
+var timerEl = document.querySelector('.timer')
+var printQuizEl = document.querySelector('.startQuiz')
+
 
 
 //arrays of arrays of questions and their answers. easy indexing 
 var completeQuiz = [
-['Commonly used types of data DO NOT include:', "1. Strings", "2. Booleans", "3. Alerts", "4. Numbers", "3. Alerts"],
-['The condition in an if/else statement is enclosed within ____.', "1. Quotes", "2. Curly Brackets", "3. Parentheses", "4. Square Brackets", "2. Curly Brackets"],
-['Arrays in Javascript can be used to store ____.', "1. Numbers and Strings", "2. Other Arrays", "3. Booleans", "4. All of the Above", "4. All of the Above"]
-['A very useful tool used during development and debugging for printing content ot the debugger is:', '1. Javascript', '2. Terminal / Bash', '3. For Loops', '4. Console.log', '4. Console.log']
+    ['Commonly used types of data DO NOT include:', "1. Strings", "2. Booleans", "3. Alerts", "4. Numbers", "3. Alerts"],
+    ['The condition in an if/else statement is enclosed within ____.', "1. Quotes", "2. Curly Brackets", "3. Parentheses", "4. Square Brackets", "2. Curly Brackets"],
+    ['Arrays in Javascript can be used to store ____.', "1. Numbers and Strings", "2. Other Arrays", "3. Booleans", "4. All of the Above", "4. All of the Above"]
+    ['A very useful tool used during development and debugging for printing content ot the debugger is:', '1. Javascript', '2. Terminal / Bash', '3. For Loops', '4. Console.log', '4. Console.log']
 ]
 
 timer = 75;
@@ -17,32 +18,55 @@ questionNum = 0;
 
 
 function init() {
-//get names and scores for leaderboard page. maybe two separate functions or one not sure yet 
+    //get names and scores for leaderboard page. maybe two separate functions or one not sure yet 
 
 }
 
 //when start button is clicked the first question is printed to the screen and timer starts
 function startQuiz() {
- startButton.addEventListener('click', function(){
-     printQuestion();
-     startTimer();
- })
+    printQuizEl.addEventListener('click', function () {
+
+        printQuestion();
+        startTimer();
+    })
 }
 
 //function to print questions. will iterate thru "completeQuiz" and pull first index of each array element
 // to get questions. the index thru to print answer choices
 function printQuestion() {
- startQuiz.textContent = completeQuiz[QuestionNum][0];
 
- for (i =0; i < completeQuiz[questionNum].length; i++){
+    for (i = 0; i < completeQuiz[questionNum].length; i++) {
+        printQuizEl.textContent = completeQuiz[QuestionNum][0];
+        var selectBubble = document.createElement("button");
+        document.body.appendChild(selectBubble);
+        selectBubble.textContent = completeQuiz[QuestionNum][i+1];
+
+    }
+}
+
+//start the timer
+function startTimer() {
+    timer = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            timerEl.textContent = timer;
+        }
+        else {
+            gameOver(); /// add game over function to display scoreboard and final time
+            return;
+        }
+
+    }, 1000);
+}
+
+//checks if inputted answer is correct or not. if wrong then subtract 10 from timer
+function rightorWrong() {
 
 
- }
 }
 
 
 
-
-startButton.addEventListener("click", startQuiz); // add start button function to the button 
+startBtnEl.addEventListener("click", startQuiz); // add start button function to the button 
 
 init(); // just a call
