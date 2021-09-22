420c3097b3c137dfe1812e912de65d7970f63b8e
Giovanni Malcolm <giovannimalcolm@giovannis-mbp.lan>
2021-09-22 07:38:26 -0400

    messed up old repo doing branching activity. making a new one

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var startButton = document.querySelector(".start-button");
+
+
+function init() {
+//get names and scores for leaderboard page. maybe two separate functions or one not sure yet 
+
+}
+
+function startGame() {
+ //something for win condition?
+ //initialize timer starting time
+ //use function to start time
+ 
+}
+
+
+
+startButton.addEventListener("click", startGame); // add start button function to the button 
+
+init(); // just a call
