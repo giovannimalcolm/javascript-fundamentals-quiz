56130921322a01141f98f2d5117e9c5ed2b2fdc8
Giovanni Malcolm <giovannimalcolm@giovannis-mbp.lan>
2021-09-23 14:49:00 -0400

    pushed this twice by accident during class. about to try something so i need to ensure its saved before it doesnt work and i forget what i had before

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 var startBtnEl = document.querySelector(".start-button");
 var timerEl = document.querySelector('.timer')
 var printQuizEl = document.querySelector('.startQuiz')
+var feedback = document.querySelector('.feedback')
+var clickedAnswer = document.querySelector('.answerButton')
+var selectBubble;
 
 
 
@@ -13,8 +16,9 @@ var completeQuiz = [
     ['A very useful tool used during development and debugging for printing content ot the debugger is:', '1. Javascript', '2. Terminal / Bash', '3. For Loops', '4. Console.log', '4. Console.log']
 ]
 
+var correctAnswers = [3, 2, 4, 4];
 var timer = 75;
-var questionNum = 1;
+var questionNum = 0;
 
 
 function init() {
@@ -24,52 +28,91 @@ function init() {
 
 //when start button is clicked the first question is printed to the screen and timer starts
 function startQuiz() {
-  //  printQuizEl.addEventListener('click', function () {
+    //  printQuizEl.addEventListener('click', function () {
 
-        printQuestion();
-        startTimer();
-        
+    printQuestion();
+    startTimer();
+
 
 }
 
 //function to print questions. will iterate thru "completeQuiz" and pull first index of each array element
 // to get questions. the index thru to print answer choices
 function printQuestion() {
+console.log('var is', questionNum)
+    if (questionNum > 0) {
+        selectBubble.classList.remove('.answerButton');
+    }
 
-    for (i = 1; i < completeQuiz[questionNum].length-1; i++) {
+
+
+    for (i = 1; i < completeQuiz[questionNum].length - 1; i++) {
+        console.log(questionNum)
         printQuizEl.textContent = completeQuiz[questionNum][0];
         var selectBubble = document.createElement("button");
-        selectBubble.classList.add('start-button');
+        selectBubble.classList.add('answerButton');
+        selectBubble.setAttribute("id", i);
         document.body.appendChild(selectBubble);
         selectBubble.textContent = completeQuiz[questionNum][i];
-
+        console.log(selectBubble.className);
+        console.log(selectBubble)
     }
+
+    (function chooseAnswer() {
+        document.body.addEventListener("click", clickButtons);
+        // ^ one handler for all clicks
+        function clickButtons(evt) {
+            const from = evt.target;
+            //console.clear();
+
+            if (from.className != 'answerButton') { return; }
+            // ^check if the element clicked is one of the elements you want to handle 
+            //  if it's not one of the 'buttons', do nothing
+            console.log("you clicked " + from.id);
+            clickedAnswer = from.id;
+            console.log(clickedAnswer)
+            rightorWrong();
+
+        }
+    }())
+
 }
 
 //start the timer
 function startTimer() {
-    timer = setInterval(function () {
-        if (timer > 0) {
-            console.log(timer);
-            timer--;
-            timerEl.textContent = timer;
-        }
-        else {
-            gameOver(); /// add game over function to display scoreboard and final time
-            return;
-        }
-
-    }, 1000);
+    var timerID = setInterval(timerClick, 1000);
 }
 
+function timerClick() {
+    if (timer > 0) {
+        console.log(timer);
+        timer--;
+        timerEl.textContent = timer;
+        console.log(timerEl);
+    }
+    else {
+        gameOver(); /// add game over function to display scoreboard and final time
+        return;
+    }
+
+}
 //checks if inputted answer is correct or not. if wrong then subtract 10 from timer
 function rightorWrong() {
-
+    if (clickedAnswer == correctAnswers[questionNum]) {
+        feedback.textContent = 'Correct!'
+    }
+    else {
+        feedback.textContent = 'False!'
+    }
+    questionNum++;
+    printQuestion();
 
 }
+
 
 
 
 startBtnEl.addEventListener("click", startQuiz); // add start button function to the button 
 
 init(); // just a call
+
